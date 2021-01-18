0083cb5c84f5afc0165e74cd4a64ce9e7b9432dc
SametWindows <s.ronaer@hotmail.com>
2021-01-18 09:02:00 +0300

    Camera tracking added and sphere moves according to force ratio.

--- PokeyBallClone/Assets/Scripts/PlayerManager.cs
+++ PokeyBallClone/Assets/Scripts/PlayerManager.cs
@@ -8,71 +8,165 @@ public class PlayerManager : MonoBehaviour
     float forceAmount;
     float forceLimit;
     float normalizedForce;
+    float animationSpeed;
+    Vector3 initialSphereLocalPosition;
+    [SerializeField] float maxSpeed = 2000;
+
+    bool sphereAnim;
+    bool sphereFree;
+    bool stopSphere;
+
     Touch touch;
+    [SerializeField] GameObject stickParent;
     [SerializeField] GameObject stick;
     [SerializeField] GameObject sphere;
     Animator stickAnim;
     Rigidbody rb;
-
+    Transform sphereParent;
     // Start is called before the first frame update
     void Start()
     {
         forceAmount = 0;
         forceLimit = -200;
-        stickAnim = stick.GetComponent<Animator>();
+        stickAnim = stickParent.GetComponent<Animator>();
         rb = sphere.GetComponent<Rigidbody>();
+        initialSphereLocalPosition = sphere.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+       
         GetForce();
         AnimateStick();
+  
+    }
+
+    private void GetNextPosition()
+    {
+            rb.velocity = new Vector3(0, 0, 0);
+            rb.useGravity = false;  
+    }
+
+    private void AnimateSphere()
+    {
+        if (!sphereAnim)
+        {
+            stickParent.transform.position += new Vector3(0,0.15f, 0);
+            sphereAnim = true;
+        }
+        else
+        {
+            stickParent.transform.position-= new Vector3(0, 0.15f, 0);
+            sphereAnim = false;
+        }
     }
 
     private void AnimateStick()
     {
-        stickAnim.SetFloat("direction", normalizedForce);
+        stickAnim.SetFloat("direction", animationSpeed);
     }
 
     private void GetForce()
     {
+        if (!sphereFree)
+        {
+            if (Input.touchCount > 0)
+            {
+                touch = Input.GetTouch(0);
+                GetForceAmount();
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    SetSphereFree();
+                    AddForce();
+                    sphereParent = sphere.transform.parent.transform;
+                    sphere.transform.parent = null;
+                    stickParent.transform.parent = sphere.transform;
+                    stopSphere = true;
+                }
+            }
+        }
+        else if (stopSphere)
+        {
+            StopForce();
+            
+        }
+    }
+
+    private void GetForceAmount()
+    {
+        if (touch.phase == TouchPhase.Moved)
+        {
+            if (touch.deltaPosition.y < 0)
+            {
+                if (forceAmount > forceLimit)
+                {
+                    forceAmount += touch.deltaPosition.y;
+                    normalizedForce = forceAmount / forceLimit;
+                    animationSpeed = normalizedForce;
+                }
+               // print(normalizedForce);
+            }
+            if (touch.deltaPosition.y > 0)
+            {
+                if (forceAmount < -1)
+                {
+                    forceAmount += touch.deltaPosition.y;
+                    normalizedForce = forceAmount / forceLimit;
+                    animationSpeed = -normalizedForce;
+                }
+                //print(normalizedForce);
+            }
+        }
+    }
+
+    private void StopForce()
+    {
         if (Input.touchCount > 0)
         {
-            touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Moved)
-            {
-                if (touch.deltaPosition.y < 0)
-                {
-                    if (forceAmount > forceLimit)
-                    {
-                        forceAmount += touch.deltaPosition.y;
-                        normalizedForce = forceAmount / forceLimit;
-                    }
-                    print(normalizedForce);
-                }
-                if (touch.deltaPosition.y > 0)
-                {
-                    if (forceAmount < -1)
-                    {
-                        forceAmount += touch.deltaPosition.y;
-                        normalizedForce = -(forceAmount / forceLimit);
-                    }
-                    print(normalizedForce);
-                }
-            }
-            if (touch.phase == TouchPhase.Ended)
-            {
-                forceAmount = 0;
-                normalizedForce = 0;
-                AddForce();
-            }
+            GetNextPosition();
+            print("End");
+            Invoke("SetSphereFree", 0.2f);
+            stick.SetActive(true);
+            stopSphere = false;
+            stickParent.transform.parent = null;
+            sphere.transform.parent = sphereParent;
+            FixStickPosition();
+        }
+    }
+
+    private void FixStickPosition()
+    {
+       
+        sphere.transform.localPosition = initialSphereLocalPosition;
+        
+    }
+
+    void SetSphereFree()
+    {
+        if(sphereFree == false)
+        {
+            sphereFree = true;
+        }
+        else
+        {
+            sphereFree = false;
         }
     }
 
     void AddForce()
     {
-        rb.AddForce(Vector3.up * 1000);
-        rb.useGravity = true;
+        if(normalizedForce > 0.01f)
+        {
+            rb.AddForce(Vector3.up * maxSpeed*normalizedForce);
+            rb.useGravity = true;
+            forceAmount = 0;
+            normalizedForce = 0;
+            stick.SetActive(false);
+            animationSpeed = 0;
+            stickAnim.SetTrigger("restart");
+
+        }
     }
 }
